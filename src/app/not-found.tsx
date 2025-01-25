c94b8787a99dfa4f24327e85d88f704780b155cc
moryno <mauricenganga41@gmail.com>
2025-01-25 14:58:48 +0300

    Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,6 @@
+export default function NotFound(){
+    return <main className="mt-20 text-center">
+        <h1 className="text-7xl m-0 font-extrabold capitalize">Not found</h1>
+        <p className="text-2xl font-medium text-gray-500">Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+}
