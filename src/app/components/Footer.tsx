f0b07e6e15017bb5277f319bf457b2eec9023882
moryno <mauricenganga41@gmail.com>
2025-01-25 13:18:08 +0300

    Add footer of the application

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,21 @@
+import { FaRegCopyright } from "react-icons/fa6";
+
+const Footer = () => {
+    const year = new Date().getFullYear();
+
+  return (
+    <section className="flex items-center justify-between py-5">
+        <article className="flex items-center gap-5">
+        <span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">about</span>
+        <span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">blogs</span>
+        <span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">projects</span>
+        <span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">contact</span>
+        </article>
+        <article className="flex items-center text-sm">
+        <FaRegCopyright size={10} /><span>{ year }, Maurice Nganga. All rights reserved.</span>
+        </article>
+    </section>
+  )
+}
+
+export default Footer
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 import { FaBluesky, FaGithub, FaLinkedin, FaMedium } from "react-icons/fa6";
 import { MdDownload } from "react-icons/md";
 import { RiContactsFill } from "react-icons/ri";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
+    <>
+  
    <section className="flex">
     <article className="w-full md:w-4/6 flex flex-col gap-5">
       <Image src="/logo.png" alt="this is the logo of the website" width={40} height={40} />
@@ -13,7 +16,8 @@ export default function Home() {
       <p className="text-xl leading-relaxed xl:leading-10 tracking-tighter">I&apos;m <b>Maurice Nganga</b>. A <b>Frontend Developer</b>. I&apos;m passionate about the web and it&apos;s infinte possibilities, <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">open source</span>, sharing my experiences on <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">medium</span> and playing <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">video games</span></p>
       <p className="text-xl leading-relaxed xl:leading-10 tracking-tighter">I&apos;m currently helping to build and shaping a decentralized future for the web and internet at <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">Smart Applications International</span>.</p>
       <p className="flex items-center gap-2 text-sm">Previuosly:<span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Anza Cloud</span><span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Arity Technologies</span><span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">IntSoft (K) Ltd</span></p>
-      <div className="flex items-center gap-3"> <Link href={"#contact"} 
+      <div className="flex items-center gap-3"> 
+        <Link href={"#contact"} 
                       target="_blank"
                       rel="noopener nonreferrer"
                       className="bg-gradient-to-r from-stone-300 to-stone-600 hover:from-stone-600 hover:to-stone-300
@@ -49,5 +53,7 @@ export default function Home() {
     </article>
     <article className="w-full md:w-2/6"></article>
    </section>
+   <Footer />
+   </>
   );
 }
