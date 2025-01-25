287100f1eb2295c3f0318ffb11a1c1f84c33c771
moryno <mauricenganga41@gmail.com>
2025-01-25 13:03:52 +0300

    Initial setup of the portfolio website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,53 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaBluesky, FaGithub, FaLinkedin, FaMedium } from "react-icons/fa6";
+import { MdDownload } from "react-icons/md";
+import { RiContactsFill } from "react-icons/ri";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+   <section className="flex">
+    <article className="w-full md:w-4/6 flex flex-col gap-5">
+      <Image src="/logo.png" alt="this is the logo of the website" width={40} height={40} />
+      <h1 className="text-6xl font-bold">Hey 👋</h1>
+      <p className="text-xl leading-relaxed xl:leading-10 tracking-tighter">I&apos;m <b>Maurice Nganga</b>. A <b>Frontend Developer</b>. I&apos;m passionate about the web and it&apos;s infinte possibilities, <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">open source</span>, sharing my experiences on <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">medium</span> and playing <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">video games</span></p>
+      <p className="text-xl leading-relaxed xl:leading-10 tracking-tighter">I&apos;m currently helping to build and shaping a decentralized future for the web and internet at <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">Smart Applications International</span>.</p>
+      <p className="flex items-center gap-2 text-sm">Previuosly:<span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Anza Cloud</span><span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Arity Technologies</span><span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">IntSoft (K) Ltd</span></p>
+      <div className="flex items-center gap-3"> <Link href={"#contact"} 
+                      target="_blank"
+                      rel="noopener nonreferrer"
+                      className="bg-gradient-to-r from-stone-300 to-stone-600 hover:from-stone-600 hover:to-stone-300
+                       rounded-full p-[1px] text-sm text-white mb-10">
+                        <button className="px-3 text-xs md:px-8 py-3 md:py-4 bg-black rounded-full border-none text-center md:text-sm
+                        font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200
+                        ease-out md:font-semibold flex items-center gap-1 hover:gap-3">
+                        <span>Contact Me</span>
+                        <RiContactsFill size={16} />
+                        </button>
+                    </Link>
+                    <Link href={""} 
+                      role="button"
+                      target="_blank"
+                      rel="noopener nonreferrer"
+                      download
+                      className="flex items-center gap-1 hover:gap-3 bg-white border border-black rounded-full md:px-8 p-3 md:py-4 uppercase md:text-sm text-xs font-medium tracking-wider no-underline transition-all duration-200 ease-out
+                       text-center text-black mb-10">
+                        <span>Get Resume</span>
+                        <MdDownload size={16} />
+                    </Link></div>
+      <div className="flex items-center gap-5">
+      <FaGithub className="text-lg" />
+      <FaLinkedin className="text-lg" />
+      <FaBluesky className="text-lg" />
+      <FaMedium className="text-lg" />
+      </div>
+      <div className="flex items-center gap-5">
+        <span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">About</span>
+        <span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Blogs</span>
+        <span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Projects</span>
+      </div>
+    </article>
+    <article className="w-full md:w-2/6"></article>
+   </section>
   );
 }
