61e09f2a630e2b1b445a98859703fd410881d83a
moryno <mauricenganga41@gmail.com>
2025-02-02 11:06:28 +0300

    Modify the outlook of the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Socials from "@/components/Socials";
+import ToggleTheme from "@/components/ToggleTheme";
 import Image from "next/image";
 import Link from "next/link";
 import { MdDownload } from "react-icons/md";
@@ -6,15 +7,17 @@ import { RiContactsFill } from "react-icons/ri";
 
 export default function Home() {
   return (
-   <section className="flex">
+  <main>
+    <section className="py-5 flex justify-end"><ToggleTheme /></section>
+     <section className="flex mt-20">
     <article className="w-full md:w-4/6 flex flex-col gap-5">
       <Image src="/logo.png" alt="this is the logo of the website" width={40} height={40} />
       <h1 className="text-6xl font-bold">Hey 👋</h1>
       <p className="text-xl leading-relaxed xl:leading-10 tracking-tighter">I&apos;m <b>Maurice Nganga</b>. A <b>Frontend Developer</b>. I&apos;m passionate about the web and it&apos;s infinte possibilities, <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">open source</span>, sharing my experiences on <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">medium</span> and playing <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">video games</span></p>
       <p className="text-xl leading-relaxed xl:leading-10 tracking-tighter">I&apos;m currently helping to build and shaping a decentralized future for the web and internet at <span className=" underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500">Smart Applications International</span>.</p>
       <p className="flex items-center gap-2 text-sm">Previuosly:<span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Anza Cloud</span><span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Arity Technologies</span><span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">IntSoft (K) Ltd</span></p>
-      <div className="flex items-center gap-3"> 
-        <Link href={"#contact"} 
+      <div className="flex mt-5 items-center gap-3"> 
+        <Link href={"/contact"} 
                       target="_blank"
                       rel="noopener nonreferrer"
                       className="bg-gradient-to-r from-stone-300 to-stone-600 hover:from-stone-600 hover:to-stone-300
@@ -36,14 +39,15 @@ export default function Home() {
                         <span>Get Resume</span>
                         <MdDownload size={16} />
                     </Link></div>
-  <Socials />
-      <div className="flex items-center gap-5">
-        <span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">About</span>
-        <span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Blogs</span>
-        <span className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Projects</span>
+              <Socials />
+      <div className="flex mt-3 items-center gap-5">
+        <Link href="/about" className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">About</Link>
+        <Link href="/blogs" className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Blogs</Link>
+        <Link href="/projects" className="underline underline-offset-8 hover:bg-red-100 hover:decoration-4 cursor-pointer decoration-pink-500 font-normal">Projects</Link>
       </div>
     </article>
     <article className="w-full md:w-2/6"></article>
    </section>
+  </main>
   );
 }
