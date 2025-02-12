0f3e3ff185f02a6fb8c3b888e65fa4cd3c073d5d
moryno <mauricenganga41@gmail.com>
2025-02-12 16:00:13 +0300

    Update the contact page

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -18,10 +18,14 @@ const ContactForm = () => {
     setIsLoading(true);
     emailjs
       .sendForm(
-        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
-        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        // process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+        // process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        // formRef.current!,
+        // process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+        "service_skvzar1",
+        "template_2ox9rmi",
         formRef.current!,
-        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+        "2zKK-EKLfrPe0NirY"
       )
       .then(
         () => {
