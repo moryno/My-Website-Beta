337a4af81d5e941adf68ee37bb94782b0135bfce
moryno <mauricenganga41@gmail.com>
2025-02-12 05:44:04 +0300

    Add swiper to the project images

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -1,41 +1,75 @@
 "use client"
 
-import Image from "next/image";
 import { useState } from "react";
+import Image from "next/image";
+
+import { Swiper as SwiperType } from "swiper"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Navigation, Pagination, Thumbs } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
 
 
 const ProductImages = ({ images } : { images : string[]}) => {
-    const [index, setIndex] = useState(0);
+    const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null);
+
 
   return (
-    <div>
-        <div className="h-[500px] relative">
-        <Image
-          src={images[index]}
-          alt="This is a prodcuct cover"
-          fill
-          sizes="50vw"
-          className="aspect-square rounded-md shadow-md"
-        />
-        </div>
-        <div className="flex justify-between gap-4 mt-5 overflow-hidden">
-        {images.map((item, i) => (
-          <div
-            className="w-1/4 h-32 relative gap-4 mt-5 cursor-pointer"
+    <>
+    <Swiper
+      loop={true}
+      spaceBetween={10}
+      navigation={true}
+      thumbs={{ swiper: thumbsSwiper }}
+      modules={[FreeMode, Navigation, Thumbs]}
+      className="h-[520px] relative"
+    >
+      {images.map((item, i) => (
+          <SwiperSlide 
             key={i}
-            onClick={() => setIndex(i)}
-          >
+             className="relative mt-5 cursor-pointer"
+            >
+            <Image
+              src={item}
+              alt="This is a prodcuct cover"
+                    fill
+                    sizes="70vw"
+                    className="aspect-square rounded-md shadow-lg"
+            />
+          </SwiperSlide>
+         
+        ))}
+    </Swiper>
+    <Swiper
+      onSwiper={setThumbsSwiper}
+      loop={true}
+      spaceBetween={10}
+      slidesPerView={4}
+      freeMode={true}
+      watchSlidesProgress={true}
+      modules={[FreeMode, Pagination, Thumbs]}
+      className="h-36"
+    >
+           {images.map((item, i) => (
+          <SwiperSlide 
+            key={i}
+             className="relative mt-5 cursor-pointer"
+            >
             <Image
               src={item}
               alt="This is product images"
               fill
               sizes="30vw"
-              className="aspect-auto rounded-md shadow-md"
+              className="aspect-auto rounded-md shadow-lg"
             />
-          </div>
+          </SwiperSlide>
+         
         ))}
-      </div>
-    </div>
+    </Swiper>
+  </>
   )
 }
 
